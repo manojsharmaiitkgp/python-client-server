e993c3349b1c16ba6d59477fa573a15579a26896
unknown <manojsharma.net@gmail.com>
2020-12-06 23:29:07 +0530

    add file

---
+++ newClient.py
@@ -0,0 +1,29 @@
+# Echo client program
+import socket
+import sys
+
+
+HOST = 'workstation'    # The remote host
+PORT = 52345              # The same port as used by the server
+s = None
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        s = None
+        continue
+    try:
+        s.connect(sa)
+    except socket.error as msg:
+        s.close()
+        s = None
+        continue
+    break
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+s.send(b'Hello, world')
+data = s.recv(1024)
+s.close()
+print('Received', repr(data))
---
+++ readme.txt.txt
@@ -0,0 +1,2 @@
+Run the server on host machine
+and use client file to connect the server
